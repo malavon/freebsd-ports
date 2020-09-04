a8c1df8a30ce592a271dbb365791c3967e4bc045
Benny Goemans <12911845+malavon@users.noreply.github.com>
2020-09-04 23:14:39 +0200

    temp first trying to fix isboot

--- net/isboot-kmod/files/patch-isboot.c
+++ net/isboot-kmod/files/patch-isboot.c
@@ -1,6 +1,21 @@
 --- isboot.c.orig	2015-11-05 16:50:51 UTC
 +++ isboot.c
-@@ -347,9 +347,9 @@ isboot_set_v4gw(struct sockaddr_in *gate
+@@ -165,11 +165,11 @@ isboot_get_ifa_by_mac(uint8_t *lladdr)
+ 
+ 	IFNET_RLOCK();
+ #if __FreeBSD_version >= 800500
+-	TAILQ_FOREACH(ifp, &V_ifnet, if_link)
++	CK_STAILQ_FOREACH(ifp, &V_ifnet, if_link)
+ #else
+-	TAILQ_FOREACH(ifp, &ifnet, if_link)
++	CK_STAILQ_FOREACH(ifp, &ifnet, if_link)
+ #endif
+-		TAILQ_FOREACH(ifa, &ifp->if_addrhead, ifa_link) {
++		CK_STAILQ_FOREACH(ifa, &ifp->if_addrhead, ifa_link) {
+ 			if (ifa->ifa_addr->sa_family != AF_LINK)
+ 				continue;
+ 			if (memcmp(lladdr,
+@@ -347,9 +347,9 @@ isboot_set_v4gw(struct sockaddr_in *gateway)
  	netmask.sin_addr.s_addr = htonl(0);
  
  	/* delete gateway if exists */
@@ -12,7 +27,7 @@
  	if (error) {
  		if (error != ESRCH) {
  			printf("rtrequest RTM_DELETE error %d\n",
-@@ -359,9 +359,9 @@ isboot_set_v4gw(struct sockaddr_in *gate
+@@ -359,9 +359,9 @@ isboot_set_v4gw(struct sockaddr_in *gateway)
  	}
  
  	/* set new default gateway */
@@ -24,7 +39,7 @@
  	if (error) {
  		printf("rtrequest RTM_ADD error %d\n", error);
  		return (error);
-@@ -391,9 +391,9 @@ isboot_set_v6gw(struct sockaddr_in6 *gat
+@@ -391,9 +391,9 @@ isboot_set_v6gw(struct sockaddr_in6 *gateway)
  	memset(&netmask.sin6_addr, 0, 16);
  
  	/* delete gateway if exists */
@@ -36,7 +51,7 @@
  	if (error) {
  		if (error != ESRCH) {
  			printf("rtrequest RTM_DELETE error %d\n",
-@@ -403,9 +403,9 @@ isboot_set_v6gw(struct sockaddr_in6 *gat
+@@ -403,9 +403,9 @@ isboot_set_v6gw(struct sockaddr_in6 *gateway)
  	}
  
  	/* set new default gateway */
