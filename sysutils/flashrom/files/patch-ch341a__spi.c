6d67a21614aa00f77a8121223189160a0a7bcb9e
Charlie Root <root@lifeboat.malavon.lkl>
2026-07-12 21:42:01 +0000

    sysutils/flashrom: additional USB device id for CH341A/B (in UART mode)

--- sysutils/flashrom/files/patch-ch341a__spi.c
+++ sysutils/flashrom/files/patch-ch341a__spi.c
@@ -1,15 +1,23 @@
---- ch341a_spi.c.orig	2024-10-24 15:36:39 UTC
+--- ch341a_spi.c.orig	2025-07-24 01:31:52 UTC
 +++ ch341a_spi.c
-@@ -70,7 +70,7 @@
+@@ -72,7 +72,7 @@
  
  
  /* Number of parallel IN transfers. 32 seems to produce the most stable throughput on Windows. */
 -#define USB_IN_TRANSFERS 32
 +#define USB_IN_TRANSFERS 1
  
- /* We need to use many queued IN transfers for any resemblance of performance (especially on Windows)
-  * because USB spec says that transfers end on non-full packets and the device sends the 31 reply
-@@ -436,7 +436,7 @@ static int ch341a_spi_init(const struct programmer_cfg
+ struct ch341a_spi_data {
+ 	struct libusb_device_handle *handle;
+@@ -89,6 +89,7 @@ static const struct dev_entry devs_ch341a_spi[] = {
+ 
+ static const struct dev_entry devs_ch341a_spi[] = {
+ 	{0x1A86, 0x5512, OK, "Winchiphead (WCH)", "CH341A"},
++	{0x1A86, 0x5523, OK, "Winchiphead (WCH)", "CH341A"},
+ 
+ 	{0},
+ };
+@@ -426,7 +427,7 @@ static int ch341a_spi_init(const struct programmer_cfg
  
  	/* Enable information, warning, and error messages (only). */
  #if LIBUSB_API_VERSION < 0x01000106
